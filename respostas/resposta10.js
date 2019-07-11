95fc622299f8d4b6509121e243cd6432b1993b0f
Mateus <mateus_sat@hotmail.com>
2019-07-11 19:15:28 -0300

    alteração do for

--- respostas/resposta10.js
+++ respostas/resposta10.js
@@ -1,26 +1,22 @@
 let numero1 = parseFloat(prompt('Digite N1: '));
 let numero2 = parseFloat(prompt('Digite N2: '));
 let numero = '';
+let auxiliar = 0;
 
 if (numero1 > numero2) {
 
-    for (let i = numero1; i <= numero2; i++) {
-
-
-        numero += i;
-
-    }
-
-} else {
-
-    for (let i = numero2; i <= numero1; i++) {
-
-
-        numero += i;
-
-
-    }
+    auxiliar = numero1;
+    numero1 = numero2;
+    numero2 = auxiliar;
 
 }
 
-alert(numero + ' ,');
+for (let i = numero1; i <= numero2; i++) {
+
+
+    numero += i + ', ';
+
+}
+
+
+alert(numero);
