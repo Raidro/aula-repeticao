43fa82df76a52e9ad087495e6447e3b64c0a3ea5
Mateus <mateus_sat@hotmail.com>
2019-07-11 19:58:14 -0300

    ajustes finos na resposta 2

--- respostas/resposta2.js
+++ respostas/resposta2.js
@@ -1,14 +1,13 @@
 
-var nome = prompt('Digite o Usuario: ');
 
 for (let i = 0; i <= 100; i++) {
-
+    let nome = prompt('Digite o Usuario: ');
     let senha = prompt('Digite sua Senha: ');
 
     if (nome == senha) {
-        alert('Senha igual a nome do usuario! digite uma senha valida');
+        alert('Senha igual a nome do usuario! Digite uma senha valida');
     } else {
         alert('Cadastro Efetuado!');
+        break;
     }
-    
 }
