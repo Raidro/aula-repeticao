d23a5b6abbed91d6fcc9757995410ca6bb6b1e87
Mateus <mateus_sat@hotmail.com>
2019-07-11 21:17:59 -0300

    serie de fibonacci

---
+++ respostas/resposta15.js
@@ -0,0 +1,16 @@
+let atual = 0;
+let n1 = 1;
+let n2 = 0;
+
+let resultado = n2 + '\n ' + n1;
+
+for (let i = 0; i < 15; i++) {
+    
+    
+    atual = n1 + n2;
+    resultado += '\n' +  atual;
+    n2 = n1;
+    n1 = atual;
+}
+
+alert(resultado);
