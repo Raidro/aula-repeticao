e6140e570ee1bf631780422a963a47a55269c5ae
Mateus <mateus_sat@hotmail.com>
2019-07-15 19:23:38 -0300

    acresentado o else

--- respostas/resposta4.js
+++ respostas/resposta4.js
@@ -5,7 +5,7 @@ let ano = 0;
 let taxaA = 0.03;
 let taxaB = 0.015;
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
     if (paisA <= paisB) {
 
@@ -13,8 +13,13 @@ for (let i = 0; i <= 100; i++) {
         paisB += paisB * taxaB;
         ano++;
 
+    }else {
+        break;
     }
+    
 }
- 
+
 alert('Levou ' + ano + ' Anos');
+
+
 
