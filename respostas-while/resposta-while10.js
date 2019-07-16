e29c9b92dd076dcd923051bfc4b6ecb20aa12422
Mateus <mateus_sat@hotmail.com>
2019-07-16 15:04:51 -0300

    terminado atv 8,9,10

---
+++ respostas-while/resposta-while10.js
@@ -0,0 +1,22 @@
+let numero1 = parseFloat(prompt('Digite N1: '));
+let numero2 = parseFloat(prompt('Digite N2: '));
+let numero = '';
+let auxiliar = 0;
+let soma = 0;
+let i = numero1;
+
+while (numero1 > numero2) {
+
+    auxiliar = numero1;
+    numero1 = numero2;
+    numero2 = auxiliar;
+
+}
+
+while (i <= numero2) {
+
+    numero += i + ', ';
+    i++
+}
+
+alert(numero);
