13302bc85c0970e33d346cae9f8d98ab2de4bd49
Mateus <mateus_sat@hotmail.com>
2019-07-16 14:25:52 -0300

    exercicios com while de 1 a 5 feitos

---
+++ respostas-while/resposta-while2.js
@@ -0,0 +1,16 @@
+let nome = '';
+let senha = '';
+
+while (nome == senha) {
+
+
+    nome = prompt('Digite o Usuario: ');
+    senha = prompt('Digite sua Senha: ');
+    alert('Senha igual a nome do usuario! Digite uma senha valida');
+
+
+}
+
+alert('Cadastro Efetuado!');
+
+
